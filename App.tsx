e1d23358c1e39e2253833f4df27775cb62f8eab1
tri/KUMA <trikuma@hotmail.com>
2022-12-31 21:52:44 +0800

    Update position of status bar element

--- App.tsx
+++ App.tsx
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
       <StatusBar style="auto" />
+      <Text>Hello, world!</Text>
     </View>
   );
 }
